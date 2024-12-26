201e60ecfc233439bb7b6da95aa7a8fb7d0cfe52
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-26 13:05:40 +0530

    NUnit Testing on Bubble Sort

---
+++ NunitTesting/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTesting
+{
+    public class BubbleSort
+    {
+        BubbleSort(List<int> list)
+        {
+            list = list;
+        }
+        public void Bubble(List<int> list)
+        {
+            var items = new List<int>();
+            for (var i = 0; i < items.Count; i++)
+            {
+                for (var j = 0; j < items.Count - 1; j++)
+                {
+                    if (items[j] > items[j + 1])
+                    {
+                        var temp = items[j];
+                        items[j] = items[j + 1];
+                        items[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+    }
+}
