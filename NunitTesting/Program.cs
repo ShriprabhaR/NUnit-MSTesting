fb511177e69846a25fc84b4a547c41af9497f8b3
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-26 12:15:26 +0530

    Nunit Testing

---
+++ NunitTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NunitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestingMethods testingMethods = new TestingMethods();
+            //testingMethods.Add(2, 3);
+        }
+    }
+}
---
+++ NunitTesting/TestingMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTesting
+{
+    public class TestingMethods
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+
+        public bool PosOrNeg(int a)
+        {
+            bool PNResult = a < 0 ? false : true;
+            return PNResult;
+        }
+
+        public int SumOfNatNum(int a)
+        {
+            int sum = a*(a + 1) / 2;
+            return sum;
+        }
+    }
+}
